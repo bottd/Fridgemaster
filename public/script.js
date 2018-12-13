5c0cfb754b7918c56841ff1e042d85b4896ce6cd
Haley Jacobs <haleyljacobs@gmail.com>
2018-12-12 20:35:48 -0700

    Added hover to api endpoints

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 function appear(e) {
-  for (let i = 1; i < 11; i ++ ){
+  for (let i = 0; i < 12; i ++ ){
     if(e.target.classList[1].includes(`${i}`)) {
       $(`.endpoint${i}`).toggleClass('appear', `.endpoint${i}`)  
     }
@@ -18,3 +18,28 @@ $('.point9').click(appear)
 $('.point10').click(appear)
 $('.point11').click(appear)
 $('.end').click(appear)
+
+
+const mouseEnter = (e) => {
+	const className = e.currentTarget.classList[1]
+	$(`.${className}-text`).css('display', 'block');
+}
+
+const mouseLeave = (e) => {
+	const className = e.currentTarget.classList[1]
+	$(`.${className}`).css("opacity", 1)
+	$(`.${className}-text`).css('display', 'none')
+}
+
+$('.point1').hover(mouseEnter, mouseLeave)
+$('.point2').hover(mouseEnter, mouseLeave)
+$('.point3').hover(mouseEnter, mouseLeave)
+$('.point4').hover(mouseEnter, mouseLeave)
+$('.point5').hover(mouseEnter, mouseLeave)
+$('.point6').hover(mouseEnter, mouseLeave)
+$('.point7').hover(mouseEnter, mouseLeave)
+$('.point8').hover(mouseEnter, mouseLeave)
+$('.point9').hover(mouseEnter, mouseLeave)
+$('.point10').hover(mouseEnter, mouseLeave)
+$('.point11').hover(mouseEnter, mouseLeave)
+
